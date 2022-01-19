a7856e243cc548df3920640de6250302c7967698
quanfbi2003 <48565257+quanfbi2003@users.noreply.github.com>
2022-01-19 16:02:41 +0700

    Test

--- VulnAlert.py
+++ VulnAlert.py
@@ -1,11 +1,12 @@
 import argparse
-import os
+
 from engines.vulnerability.cve import CVE
-from tools.loghandler import *
 from tools.configfile import *
 from tools.database import *
 from tools.emails import *
+from tools.loghandler import *
 from tools.telegram import Telegram
+
 
 class VulnAlert:
     def __init__(self):
@@ -15,46 +16,45 @@ class VulnAlert:
             formatter_class=argparse.RawDescriptionHelpFormatter)
 
         parser.add_argument('--config',
-            dest="configfile",
-            required=True,
-            help='Configuration directory and file name.')
-
+                            dest="configfile",
+                            required=True,
+                            help='Configuration directory and file name.')
 
         parser.add_argument('--service',
-            help="If the script is being used as a service use this parameter in conjunction with --timeleep",
-            action='store_true',
-            dest="argservice")
+                            help="If the script is being used as a service use this parameter in conjunction with "
+                                 "--timeleep",
+                            action='store_true',
+                            dest="argservice")
 
         parser.add_argument('--timesleep',
-            dest="argtimesleep",
-            help='Defines the waiting time for the next round, together with the --service parameter')
+                            dest="argtimesleep",
+                            help='Defines the waiting time for the next round, together with the --service parameter')
 
         parser.add_argument("--log",
-            dest="logLevel",
-            default='INFO',
-            choices=[
-                'DEBUG',
-                'INFO',
-                'WARNING',
-                'ERROR',
-                'CRITICAL'],
-            help="Enter the type of log you would like to view. INFO is standard.")
+                            dest="logLevel",
+                            default='INFO',
+                            choices=[
+                                'DEBUG',
+                                'INFO',
+                                'WARNING',
+                                'ERROR',
+                                'CRITICAL'],
+                            help="Enter the type of log you would like to view. INFO is standard.")
 
         parser.add_argument("--engine",
-            dest="engine",
-            choices=[
-                'cve'
-            ],
-            help="Inform which engine you want to run to start the detection process.")
+                            dest="engine",
+                            choices=[
+                                'cve'
+                            ],
+                            help="Inform which engine you want to run to start the detection process.")
 
         args = parser.parse_args()
         if args.argservice:
             while True:
-                Ingestor(args).start
+                Ingestor(args).start()
                 time.sleep(int(args.argtimesleep))
         else:
-            Ingestor(args).start
-
+            Ingestor(args).start()
 
 
 class Ingestor:
@@ -68,15 +68,16 @@ class Ingestor:
         self.logger.info("CVE")
 
         if self.config.general['proxy']['active']:
-            self.logger.info("O proxy está ativo e as informações de proxy que estão no arquivo de configurações serão utilizadas.")
+            self.logger.info(
+                "O proxy está ativo e as informações de proxy que estão no arquivo de configurações serão utilizadas.")
             os.environ['http_proxy'] = f"http://{self.config.general['proxy']['ip']}" \
-               + f":{self.config.general['proxy']['port']}"
+                                       + f":{self.config.general['proxy']['port']}"
             os.environ['HTTP_PROXY'] = f"https://{self.config.general['proxy']['ip']}" \
-                + f":{self.config.general['proxy']['port']}"
+                                       + f":{self.config.general['proxy']['port']}"
             os.environ['https_proxy'] = f"https://{self.config.general['proxy']['ip']}" \
-                + f":{self.config.general['proxy']['port']}"
+                                        + f":{self.config.general['proxy']['port']}"
             os.environ['HTTPS_PROXY'] = f"https://{self.config.general['proxy']['ip']}" \
-                + f":{self.config.general['proxy']['port']}"
+                                        + f":{self.config.general['proxy']['port']}"
 
     @property
     def start(self):
@@ -98,7 +99,7 @@ class Ingestor:
                 self.config
             ), self.config
         )
-        
+
         telegram = Telegram(
             self.args,
             LoggerHandler(
@@ -108,7 +109,7 @@ class Ingestor:
             ),
             self.config
         )
-        
+
         if self.args.engine is not None:
             if str(self.args.engine).lower() == 'cve':
                 CVE(LoggerHandler(self.args.logLevel, "CVE", self.config),
@@ -116,7 +117,7 @@ class Ingestor:
                     self.args,
                     database,
                     email,
-                    telegram).start
+                    telegram).start()
             else:
                 self.logger.error(f"{self.args.engine} does not correspond to CVE")
 
--- tools/loghandler.py
+++ tools/loghandler.py
@@ -1,17 +1,16 @@
-import os
 import logging
 from pathlib import Path
-from datetime import datetime
-import subprocess
 
-class LoggerHandler():
-    def __init__(self, level, logname:str, config:dict):
+
+class LoggerHandler:
+    def __init__(self, level, logname: str, config: dict):
+        self.config = config
         self.level = getattr(logging, level)
         self.logger = logging.getLogger(logname)
         self.logger.setLevel(self.level)
         ch = logging.StreamHandler()
         ch.setLevel(self.level)
-        logFile = Path(__file__).parents[1]
+        Path(__file__).parents[1]
 
         formatter_console = logging.Formatter(
             '[%(asctime)s] - %(levelname)s - %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
